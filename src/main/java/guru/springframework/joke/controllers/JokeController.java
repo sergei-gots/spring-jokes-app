a2cc5a2269fba0be6ebf52804c4e02f7bad37c6e
sergei <sergei.gots@yahoo.com>
2025-05-23 15:30:05 +0300

    @GetMapping instead of @RequestMapping

--- src/main/java/guru/springframework/joke/controllers/JokeController.java
+++ src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -4,7 +4,7 @@ import guru.springframework.joke.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by sergei on 23/05/2025
@@ -19,7 +19,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/", ""})
+    @GetMapping({"/", "/chuck"})
     public String showJoke(Model model) {
 
         model.addAttribute("joke", jokeService.getJoke());
