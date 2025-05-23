24ada2411ee0b770360f54de1c997e6c09138b19
sergei <sergei.gots@yahoo.com>
2025-05-23 14:02:13 +0300

    JokeServiceImpl added

---
+++ src/main/java/guru/springframework/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sergei on 23/05/2025
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
